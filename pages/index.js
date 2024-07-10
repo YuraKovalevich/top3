f15261e84833b8c409fff8941ed0960f6fb42557
Yury Kovalevich <kovalevichyura@gmail.com>
2024-07-10 09:16:40 +0300

    add active element in blub

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,14 @@
 function selectLanguage(language) {
     document.getElementById('selected-language').textContent = language;
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const elements = document.querySelectorAll('.scroll__el');
+
+    elements.forEach(element => {
+        element.addEventListener('click', () => {
+            elements.forEach(el => el.classList.remove('active'));
+            element.classList.add('active');
+        })
+    })
+})
